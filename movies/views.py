adc89f5d8185744090caef1a245076242ccb1fb2
anzani-hun <anzani@hun>
2024-01-15 18:28:58 +0100

    ora_vege

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
+from .models import Movie
+from .serializers import MovieSerializer
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
 # Create your views here.
+
+@api_view(['GET'])
+def getMovies(request):
+    movies = Movie.objects.all()
+    serialized = MovieSerializer(movies, many = True)           # lista miatt kell many=True
+
+    return Response(serialized.data)
